db89652e6707891179fd1e0dc41ea519b775ca79
rakib883 <sheikhrakib883@gmail.com>
2024-12-25 02:29:32 +0600

    last

--- admin/src/ui/Border.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Border = () => {
-  return (
-    <div className="  border-[.5px]"></div>
-  )
-}
-
-export default Border
--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -5,20 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily:{
-         mainFont:['Inter',"sans-serif"]
-      },
-      screens: {
-        // Custom Breakpoints
-        xs: '320px',   // Extra small
-        sm: '640px',   // Small
-        md: '768px',   // Medium
-        lg: '1024px',  // Large
-        xl: '1280px',  // Extra large
-        '2xl': '1536px', // 2XL
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
